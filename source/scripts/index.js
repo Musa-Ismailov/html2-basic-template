684af312495d9be4ac6872475c131739ca22beca
Musa-Ismailov <164714080+Musa-Ismailov@users.noreply.github.com>
2026-06-08 20:33:45 +0300

    Стилизует кнопки чекбоксы и радиокнопки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,23 @@
-/* в этот файл добавляет скрипты*/
+const sliderElement = document.querySelector('.catalog__slider');
+const minValueElement = document.querySelector('.catalog__value--min');
+const maxValueElement = document.querySelector('.catalog__value--max');
+
+if (sliderElement.noUiSlider) {
+  sliderElement.noUiSlider.destroy();
+}
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 1000,
+  },
+  start: [0, 900],
+  step: 1,
+  connect: true,
+});
+
+sliderElement.noUiSlider.on('update', (values) => {
+
+  minValueElement.value = Math.round(values[0]);
+  maxValueElement.value = Math.round(values[1]);
+});
